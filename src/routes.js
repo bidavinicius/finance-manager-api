ca9b51145d0bc9afed141872957af4ecea5ff6bc
Vinicius Ribas Bida <bidaviniciusribas@gmail.com>
2026-04-04 08:50:29 +0300

    Changing

--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,6 @@ import { authMiddleware } from './middlewares/authMiddleware.js';
 const router = Router();
 const universalController = new UniversalController();
 
-// --- ROTA 1: AUTH (Pública) ---
-// Serve para Login e Registro
 router.post('/auth', (req, res) => universalController.handleAuth(req, res));
 
 // --- ROTA 2: DADOS (Privada) ---
